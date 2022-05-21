0ba85c366c75eac744696a9903b70c56e64ec825
paul chen <paupauchinunu@pauls-MacBook-Pro.local>
2022-05-20 18:09:15 -0700

    random cat pic from online

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import MainContent from "./MainContent";
 import CardsSlide from "./CardsSlide";
 import Form from "./Form";
 import RandomKatie from "./KatieMeme";
+import RandomCat from "./RandomCat";
 
 export default function App() {
   const [userName, setUserName] = useState({ firstName: "", lastName: "" });
@@ -17,6 +18,7 @@ export default function App() {
     <div className="container">
       <Header />
       <main>
+        <RandomCat />
         <Form handleForm={handleForm} />
         <MainContent userName={userName} />
         <RandomKatie />
